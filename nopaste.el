cda0eba757565d1febce6fe0d4aa567ec3024195
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-28 16:24:24 +0000

    Steal the idea of nopaste-last-url from jrockway's nopaste.el

--- nopaste.el
+++ nopaste.el
@@ -61,14 +61,16 @@
 (defvar nopaste-prev-channel nil
   "The last channel provided or `nil' if none. For internal use")
 
+(defvar nopaste-last-url nil "The last URL from the paste server")
+
 (defun nopaste-buffer (start end)
-  "wrapper" 
+  "`mark-whole-buffer' and nopaste it." 
   (interactive "r")
   (mark-whole-buffer)
   (nopaste-region (point-min) (point-max)))
 
 (defun nopaste-region (start end &optional nickname description channel)
-  ""
+  "nopaste the currently selected region."
   (interactive "r")
   (let* ((nickname (or nickname nopaste-nickname  (read-from-minibuffer "Nick: " nopaste-nickname)))
         (description (and nopaste-description (read-from-minibuffer "Description: " nopaste-prev-description)))
@@ -101,8 +103,14 @@
 
           (set-buffer temp-buffer-name)
           (let ((url (chomp (buffer-string))))
-            (message "Got URL %s from nopaste" url)
-            (kill-new url)))))))
+            (message "Got URL %s from nopaste, use `nopaste-yank-url' to grab it" url)
+            (setq nopaste-last-url url)))))))
+
+
+(defun nopaste-yank-url ()
+  "Insert the URL of the last nopaste at point"
+  (interactive)
+  (insert nopaste-last-url))
 
 ;; From http://github.com/al3x/emacs/blob/cdbd57f589f967efa5e9d4c83e88497db0fd71f9/utilities/chomp.el
 (defun chomp (str)
