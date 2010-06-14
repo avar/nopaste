dd21ca4f6b1e4cb45b23ef89d97056bd09f518bc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-06-14 19:37:16 +0000

    Make nopaste.el M-x checkdoc clean

--- nopaste.el
+++ nopaste.el
@@ -39,51 +39,63 @@
 ;; (global-set-key (kbd "C-c n p") 'nopaste)
 ;; (global-set-key (kbd "C-c n y") 'nopaste-yank-url)
 
+
+;;; History:
+;; 
+
 ;;; Code:
 
 
 ;; Public variables
 (defvar nopaste-nickname ""
-  "The nick given to nopaste")
+  "The nick given to nopaste.")
 (defvar nopaste-description ""
-  "The description given to nopaste")
+  "The description given to nopaste.")
 (defvar nopaste-channel ""
-  "The channel given to nopaste")
+  "The channel given to nopaste.")
 (defvar nopaste-language ""
-  "The the language given to nopaste")
+  "The the language given to nopaste.")
 (defvar nopaste-service ""
-  "The nopaste service to use. This can also be set through the
-  NOPASTE_SERVICES environmental variable to be read by nopaste
+  "The nopaste service to use.
+This can also be set through the
+NOPASTE_SERVICES environmental variable to be read by nopaste
   itself.")
 
 (defvar nopaste-command "nopaste"
-  "The nopaste command name. Will use `nopaste' in your system's
-  $PATH by default")
+  "The nopaste command name.
+Will use `nopaste' in your system's $PATH by default")
 
 ;; Internal variables
 (defvar nopaste-prev-description ""
-  "The last description provided. For internal use")
+  "The last description provided.  For internal use.")
 (defvar nopaste-prev-channel nil
-  "The last channel provided or `nil' if none. For internal use")
+  "The last channel provided or nil if none.  For internal use.")
 
-(defvar nopaste-last-url nil "The last URL from the paste server")
+(defvar nopaste-last-url nil "The last URL from the paste server.")
 (defvar nopaste-kill-last-url t
-  "Whether to `kill-new' the URL we get to make it available in
-the kill ring")
+  "Whether to make the URL we get available in the kill ring.")
 
 (defun nopaste (&optional start end nickname description channel)
-  "Nopaste either the currently active region or the entire
-buffer. If `region-active-p' is true we'll paste the region,
-otherwise we'll paste the buffer from `point-min' to
-`point-max'."
+   "Shell out to the nopaste(1) program with the current region or buffer.
+
+Nopastes either the currently active region from `START' to
+`END', or the entire buffer.  If `region-active-p' is true we'll
+paste the region, otherwise we'll paste the buffer from
+`point-min' to `point-max'.
+
+Other optional arguments are `NICKNAME' `DESCRIPTION' and
+`CHANNEL'."
   (interactive "r")
   (when (not (region-active-p))
     (setq start (point-min) end (point-max)))
   (nopaste-region start end nickname description channel))
 
 (defun nopaste-region (start end &optional nickname description channel)
-  "Nopaste a given region, nopaste will be called with
-`call-process-region'."
+  "Nopaste a given region, nopaste will be called with `call-process-region'.
+
+The arguments are like the arguments to `nopaste', except `START'
+and `END' aren't optional, i.e it also takes `NICKNAME' `DESCRIPTION' and
+`CHANNEL'"
   (interactive "r")
   (let* ((nickname (or nickname nopaste-nickname  (read-from-minibuffer "Nick: " nopaste-nickname)))
         (description (and nopaste-description (read-from-minibuffer "Description: " nopaste-prev-description)))
@@ -111,8 +123,8 @@ otherwise we'll paste the buffer from `point-min' to
             (if (numberp exit-value)
               (cond
                ((eq 0 exit-value))
-               (t (error "nopaste failed with exit value %d" exit-value)))
-              (error "nopaste failed failed: %s" exit-value)))
+               (t (error "Error: nopaste failed with exit value %d" exit-value)))
+              (error "Error: nopaste failed failed: %s" exit-value)))
 
           (set-buffer temp-buffer-name)
           (let ((url (-nopaste-chomp (buffer-string))))
@@ -123,12 +135,12 @@ otherwise we'll paste the buffer from `point-min' to
 
 
 (defun nopaste-yank-url ()
-  "Insert the URL of the last nopaste at point"
+  "Insert the URL of the last nopaste at point."
   (interactive)
   (insert nopaste-last-url))
 
 (defun -nopaste-chomp (str)
-  "If there's a \n$, kill it"
+  "Private: Takes a `STR' and chomps its trailing \n$."
   (if (equal (elt str (- (length str) 1)) ?\n)
       (substring str 0 (- (length str) 1))
     str))
